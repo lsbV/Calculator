794344d1dacb6bfe4278dc298ee5ad86b7a1d831
Shnay_rz45 <Shnay_rz45@student.itstep.org>
2023-10-19 10:04:58 +0300

    Add Error and Exception classes

---
+++ AnalaizerClass/CalcException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalaizerClass
+{
+    public class CalcException : Exception
+    {
+        public CalcException(string message) : base(message)
+        {
+        }
+    }
+}
