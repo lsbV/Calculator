c1423022cfa92ac00eddd222f444e7407ebfbdd0
Shnay_rz45 <Shnay_rz45@student.itstep.org>
2023-10-21 10:37:07 +0300

    Analyzer v1.0

--- AnalaizerClass/MathError.cs
+++ AnalaizerClass/MathError.cs
@@ -9,8 +9,8 @@ namespace AnalaizerClass
     public static class MathError
     {
         public static string Error01 { get; } = "Error 01 at <i> — Неправильна структура в дужках, помилка на <i> символі.";
-        public static string Error02 { get; } = "Error 03 — Невірна синтаксична конструкція вхідного виразу.";
-        public static string Error03 { get; } = "Error 02 at <i> — Невідомий оператор на <i> символі.";
+        public static string Error02 { get; } = "Error 02 at <i> — Невідомий оператор на <i> символі.";
+        public static string Error03 { get; } = "Error 03 — Невірна синтаксична конструкція вхідного виразу.";
         public static string Error04 { get; } = "Error 04 at <i> — Два підряд оператори на <i> символі.";
         public static string Error05 { get; } = "Error 05 — Незавершений вираз.";
         public static string Error06 { get; } = "Error 06 — Дуже мале, або дуже велике значення числа для int.";
